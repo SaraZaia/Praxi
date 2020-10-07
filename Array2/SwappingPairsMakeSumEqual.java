edbb632f3c41b37b19c534630ce5546c7f057f96
Sara Zaia <sarazaiaaa@gmail.com>
2020-10-07 06:00:15 +0200

    Update SwappingPairsMakeSumEqual.java

--- Array2/SwappingPairsMakeSumEqual.java
+++ Array2/SwappingPairsMakeSumEqual.java
@@ -2,6 +2,7 @@
 
 class SwappingPairsMakeSumEqual
 {
+    //Using SET<Integer> to look for 2 numbers in A and B, that the difference is equal to diffAtoB
     long findSwapValues(long A[], int n, long  B[], int m)
     {
         Set<Long> setA = new HashSet<>();
@@ -19,6 +20,34 @@ class SwappingPairsMakeSumEqual
         long diffAtoB = avg - sumA;
         
         for(long x: B) if(setA.contains(x + diffAtoB )) return 1;
+        return 0;
+    }
+    
+    //Using sort to look for 2 numbers in A and B, that the difference is equal to diffAtoB
+    long findSwapValuesSort(long A[], int n, long  B[], int m)
+    {
+        long sumA = 0;
+        long sumB = 0;
+        
+        for(long x : A) {
+            sumA +=x;
+        }
+        for(long x : B) {
+            sumB +=x;
+        }
+        long avg = (sumA+sumB) / 2;
+        long diffAtoB = avg - sumA;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int startA = 0;
+        int endB = m-1;
+        while(startA<n && endB >=0) {
+            if( A[startA] + diffAtoB < B[endB]) startA++;
+            else if( A[startA] + diffAtoB > B[endB]) endB--;
+            else return 1;
+            
+        }
         return 0;
     }
 }
