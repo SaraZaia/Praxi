129a38023bff2bb5f3e545207d65708f2f67520a
Sara Zaia <sarazaiaaa@gmail.com>
2020-10-09 02:14:03 +0200

    Create SortColor.java

---
+++ Array3/SortColor.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length - 1;
+        int indexStart = 0;
+        int indexEnd = n;
+        int i = 0;
+        while(i <= indexEnd) {
+            if(nums[i] == 0) {
+                swap(nums, i, indexStart);
+                indexStart++;
+                i++;
+            } else if(nums[i] == 2) {
+                swap(nums, i, indexEnd);
+                indexEnd--;
+            } else{
+                i++;
+            }
+        }
+    }
+    
+    
+    public void swap(int[] nums, int iOne, int iTwo) {
+        int temp = nums[iOne];
+        nums[iOne] = nums[iTwo];
+        nums[iTwo] = temp;
+    }
+}
