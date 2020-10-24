f56ea5bb439ce735e2703b74b4f000e195c9590b
Sara Zaia <sarazaiaaa@gmail.com>
2020-10-25 00:18:10 +0100

    Update DeleteOperationForTwoStrings.java

--- String/DeleteOperationForTwoStrings.java
+++ String/DeleteOperationForTwoStrings.java
@@ -1,7 +1,26 @@
 //https://leetcode.com/problems/delete-operation-for-two-strings/
 
 class DeleteOperationForTwoStrings {
+    
+    
+    //Dynamic Programming
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        int lcs = 0;
+        
+        for(int i = 1; i <= word1.length(); i++) {
+            for(int j = 1; j <= word2.length(); j++) {
+                if(word1.charAt(i-1)== word2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                System.out.println("word1 " + word1.charAt(i-1) + " word2 " + word2.charAt(j-1)+ " == " +dp[i][j]);
+            }
+            System.out.println(" --------------------------------- ");
+        }
+        lcs = dp[word1.length()][word2.length()];
+        return word1.length() -2*lcs + word2.length();
+    }
 
+    //DFS without memorisation
     public int minDistance(String word1, String word2) {
         
         int lcs = lcs(word1, word2, 0, 0);
@@ -9,8 +28,6 @@ class DeleteOperationForTwoStrings {
         
         return word1.length() -2*lcs + word2.length();
     }
-    
-    
     //LongestCommon subsequence
     public int lcs(String s1, String s2,int indexS1, int indexS2) {
         
