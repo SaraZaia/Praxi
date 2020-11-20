1dae09715aeb02544ebe78f0d26e6813e1e3f09a
Sara Zaia <sarazaiaaa@gmail.com>
2020-11-20 23:44:58 +0100

    Update MedianFinder.java

--- Heap/MedianFinder.java
+++ Heap/MedianFinder.java
@@ -1,6 +1,55 @@
 //https://leetcode.com/problems/find-median-from-data-stream/
 
 
+// Use Two heaps: Max heap {1,3,5} and Min heap {7,9} ===> 5
+// Max heap {1,3,5} and Min heap {7,9,12} ===> 5 + 7 / 2 = 6
+class MedianFinder {
+    
+    PriorityQueue<Integer> minQueue;
+    PriorityQueue<Integer> maxQueue;
+    
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        minQueue = new PriorityQueue<>();
+        maxQueue = new PriorityQueue<>(Collections.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        if(!minQueue.isEmpty() &&  minQueue.peek() < num) minQueue.add(num);
+        else if(!maxQueue.isEmpty() &&  maxQueue.peek() >= num) maxQueue.add(num);
+        else minQueue.add(num);
+        
+        if(maxQueue.size() > minQueue.size() + 1) minQueue.add(maxQueue.poll());
+        if(maxQueue.size() < minQueue.size()) maxQueue.add(minQueue.poll());
+
+        
+    }
+    
+    public double findMedian() {
+        int n = minQueue.size() + maxQueue.size();
+        double result;
+        if(n == 0) result = 0;
+        else if(n % 2 == 0) result = (maxQueue.peek() + minQueue.peek()) / 2.0;
+        else result = maxQueue.peek() ;
+        return result;       
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
+
+
+
+
+
+
+
+//Using ArrayList + Insertion sort + Binary Search 
 class MedianFinder {
 
     static List<Integer> list;
