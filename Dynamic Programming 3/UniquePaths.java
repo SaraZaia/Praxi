7d738ccc77f32b40522117ccf524395a90838589
Sara Zaia <sarazaiaaa@gmail.com>
2020-12-06 04:19:40 +0100

    Update UniquePaths.java

--- Dynamic Programming 3/UniquePaths.java
+++ Dynamic Programming 3/UniquePaths.java
@@ -1,13 +1,21 @@
 //https://leetcode.com/problems/unique-paths/
 
-
-class UniquePaths {
-/*    public int uniquePaths(int m, int n) {
+// Recursion + memoization
+//---------------------------------------------------
+    Map<String, Integer> tab = new HashMap<>();
+    public int uniquePaths(int m, int n) {
+        if(tab.containsKey(Math.min(m,n) + " - " + Math.max(m,n))) 
+            return tab.get(Math.min(m,n) + " - " + Math.max(m,n));
         if(n==1 || m==1) return 1; 
-        return  uniquePaths(m-1, n) + uniquePaths(m, n-1);
-    }*/
+        int result = uniquePaths(m-1, n) + uniquePaths(m, n-1);
+        tab.put(Math.min(m,n) + " - " + Math.max(m,n), result);
+        return result;
+    }
+
+// Tabulation
+//--------------------------------------------------------------------------------
     
-    public int uniquePaths(int line, int col) {
+    public int uniquePathsTabulation(int line, int col) {
         int[][] tab = new int[line][col];
         
         for(int i = 0; i < line; i++) {
@@ -18,4 +26,5 @@ class UniquePaths {
         }
      return tab[line-1][col-1];
     }
+
 }                                                                           
