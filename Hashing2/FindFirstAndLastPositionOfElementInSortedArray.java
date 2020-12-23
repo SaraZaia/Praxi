35b8e278844c3ae9d70625978bc24f492639e40c
Sara Zaia <sarazaiaaa@gmail.com>
2020-12-24 00:43:11 +0100

    Update FindFirstAndLastPositionOfElementInSortedArray.java

--- Hashing2/FindFirstAndLastPositionOfElementInSortedArray.java
+++ Hashing2/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,6 +1,42 @@
 //https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/
 
 class FindFirstAndLastPositionOfElementInSortedArray {
+    
+    public int[] searchRangeBinarySearch(int[] nums, int target) {
+        
+        int pStart = 0;
+        int pEnd = nums.length - 1;
+        int mid;
+        int[] result = new int[]{-1,-1};
+
+        
+        while (pStart <= pEnd) {
+            mid = pStart + (pEnd - pStart)/2;
+
+            if(result[0] == -1) {
+                if(nums[mid] == target && (mid == 0 || nums[mid-1] < target)) {
+                    result[0] = mid;
+                    result[1] = mid;
+                    pStart = mid + 1;
+                    pEnd = nums.length - 1;
+                }
+                else if(nums[mid] < target) pStart = mid + 1;
+                else pEnd = mid-1;
+            } else {
+                if(nums[mid] == target && (mid == nums.length-1 || nums[mid+1] > target)) {
+                    result[1] = mid;
+                    break;
+                }
+                else if(nums[mid] == target) pStart = mid + 1;
+                else pEnd = mid - 1;                
+            }
+        }
+
+        return result;
+    }
+    
+    
+    
     public int[] searchRange(int[] nums, int target) {
         
         int pStart = 0;
