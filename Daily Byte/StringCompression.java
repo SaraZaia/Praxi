2deeb11a571f47cf1ed4c6851fd1830d24bf9ff9
Sara Zaia <sarazaiaaa@gmail.com>
2021-01-03 15:03:14 +0100

    Update StringCompression.java

--- Daily Byte/StringCompression.java
+++ Daily Byte/StringCompression.java
@@ -15,7 +15,7 @@ class StringCompression {
                       chars[pointer++] = c;  
                     }
                 } else {
-                    if(i > 0) chars[pointer++] = chars[i-1];
+                    if(i > 0) chars[pointer++] = chars[i-1];    
                 }
                 diffWindow = 1;
             } else {
@@ -30,6 +30,31 @@ class StringCompression {
         } else {
             chars[pointer++] = chars[chars.length - 1];
         }
+        return pointer;   
+    }
+    
+    //Using chars[i+1] instead of chars[i-1] make the difference
+    public int compress(char[] chars) {
+        int result = chars.length;
+        int pointer = 0;
+        int diffWindow = 1;
+        
+        for(int i = 0; i < chars.length; i++) {
+            if(i == chars.length -1 || chars[i+1] != chars[i]) {
+                if(diffWindow >= 2) {
+                    chars[pointer++] = chars[i];
+                    for(char c: (diffWindow +"").toCharArray()) {
+                      chars[pointer++] = c;  
+                    }
+                } else {
+                    chars[pointer++] = chars[i];
+                }
+                diffWindow = 1;
+            } else {
+                diffWindow++;
+            }
+        }
+
         return pointer;   
     }
 }
